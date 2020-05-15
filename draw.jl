0012f7fc623eab95b25a3cdfcdd31dbba87bb85b
norheim <norheim@mit.edu>
2020-05-15 11:11:12 +0200

    change goal region color to green

--- draw.jl
+++ draw.jl
@@ -3,7 +3,7 @@ rectangle(w, h, x, y) = Shape(ones(4)*x + [0,w,w,0], ones(4)*y + [0,0,h,h])
 
 function plot_goal(xN, ϵ)
     plot!(rectangle(2*ϵ[1], 2*ϵ[3], xN[1]-ϵ[1], xN[3]-ϵ[3]),
-        opacity=.5, legend=:none)
+        fillcolor = plot_color(:green, 0.3), legend=:none)
 end
 
 function plot_polygons(polygons)
